c1f5722db84310ee3f76cd5dcfc1c030716c345f
Whistner Rojas <whistner.rojas.fg@gmail.com>
2023-03-23 06:13:28 +0800

    added linkedin

--- src/pages/components/home/socialLink.tsx
+++ src/pages/components/home/socialLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaFacebook, FaGithub, FaTelegram} from 'react-icons/fa'
+import {FaFacebook, FaGithub, FaTelegram, FaLinkedin} from 'react-icons/fa'
 import {AiFillGoogleCircle} from 'react-icons/ai'
 
 export default function SocialLink() {
@@ -7,6 +7,7 @@ export default function SocialLink() {
         <span className='fs-2 social'>
             <a href="https://web.facebook.com/whistner.R" target="_blank" rel='noreferrer'><FaFacebook /></a>
             <a href="https://github.com/WhistnerRojas" target="_blank" rel='noreferrer noopener'><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/whistnerrojas/" target="_blank" rel='noreferrer noopener'><FaLinkedin /></a>
             <a href="https://t.me/OneP1nch" target="_blank" rel='noreferrer'><FaTelegram /></a>
             <a href="https://developers.google.com/profile/u/MrRojas" style={{fontSize: 36}} target="_blank" rel='noreferrer'><AiFillGoogleCircle /></a>
         </span>

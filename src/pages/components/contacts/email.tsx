1921a38ca16fbfe13cbbf0fa68d65909015ff900
Whistner Rojas <whistner.rojas.fg@gmail.com>
2023-03-26 18:41:47 +0800

    added contact function

--- src/pages/components/contacts/email.tsx
+++ src/pages/components/contacts/email.tsx
@@ -3,6 +3,10 @@ import { Button, Box } from '@mui/material';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { contactFormSchema } from '../contacts/contactFormSchema';
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
+// import { string } from 'yup';
 
 interface FormData {
     fullName: string;
@@ -10,69 +14,111 @@ interface FormData {
     message: string;
 }
 
+export interface State extends SnackbarOrigin {
+    open: boolean;
+    msg: string;
+}
 
 export const Email = () => {
+
+    const [state, setState] = useState<State>({
+        open: false,
+        vertical: "top",
+        horizontal: "center",
+        msg: ''
+    });
+    const { vertical, horizontal, open, msg } = state;
+
+    const handleClose = ()=>{
+        setState({ ...state, open: false });
+    }
+
+
     const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
         resolver: yupResolver(contactFormSchema)
     });
 
+    const form= useRef<HTMLFormElement>()
+
     const onSubmit = (data: FormData) => {
-        console.log(data);
+        // setState({ ...state, open: true, msg: 'Message sent!' })
+        if(form && form.current){
+            emailjs.sendForm('service_erdiz4k', 'template_23rrysf', form.current, '0WnMJ0BiL31tAZwFW')
+            .then((result) => {
+                result.text === 'OK' ? setState({ ...state, open: true, msg: 'Message sent!' }) : setState({ ...state, open: true, msg: 'There was a problem while sending your email. Please try again.' });
+                form.current?.reset()
+            }, (error) => {
+                console.log(error.text);
+            });
+        }
     };
 
     return (
-        
-        <Box
-            component='form'
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{
-                '& .MuiInputLabel-root': {color: '#f0f0f0'},
-                '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                    borderColor: 'white', // Change this to the desired color
+        <>
+            <div>
+                <Snackbar
+                    anchorOrigin={{ vertical, horizontal }}
+                    autoHideDuration={3000}
+                    open={open}
+                    onClose={handleClose}
+                    message={msg}
+                    key={vertical + horizontal}
+                />
+            </div>
+            <Box
+                component='form'
+                ref={form}
+                onSubmit={handleSubmit(onSubmit)}
+                sx={{
+                    '& .MuiInputLabel-root': {
+                        color: '#f0f0f0 !important',
                     },
-                    '&:hover fieldset': {
-                        borderColor: 'rgb(0, 195, 255)', // Change this to the desired hover color
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                        borderColor: 'white', // Change this to the desired color
+                        },
+                        '&:hover fieldset': {
+                            borderColor: 'rgb(0, 195, 255)', // Change this to the desired hover color
+                        },
+                        '&:focus fieldset': {
+                            borderColor: 'rgb(0, 195, 255)', // Change this to the desired hover color
+                        },
+                        // width : '30ch', //change width of input 
+                        backgroundColor: '#303030',
                     },
-                    '&:focus fieldset': {
-                        borderColor: 'rgb(0, 195, 255)', // Change this to the desired hover color
-                        color: '#fff'
+                    '& .MuiFormHelperText-root': {
+                        fontSize: '14px',
                     },
-                    // width : '30ch', //change width of input 
-                    backgroundColor: '#303030',
-                },
-                '& .MuiFormHelperText-root': {
-                    fontSize: '14px',
-                },
-                '& .MuiTextField-root':{
-                    mt: 2
-                }
-            }}
-            noValidate
-            autoComplete="off"
-            className='d-flex justify-content-end flex-column'
-        >
-            <Contactfield 
-                label='Full name'
-                register={{...register("fullName")}}
-                error={!!errors.fullName}
-                helperText={errors.fullName?.message}
-            />
-            <Contactfield 
-                label='Email'
-                register={{...register("email")}}
-                error={!!errors.email}
-                helperText={errors.email?.message}
-            />
-            <Contactfield 
-                label='Message'
-                register={{...register("message")}}
-                error={!!errors.message}
-                helperText={errors.message?.message}
-                multiline={true}
-                rows={4}
-            />
-            <Button type="submit" variant="contained" color="primary" className='mt-3'>send message</Button>
-        </Box>
+                    '& .MuiTextField-root':{
+                        mt: 2
+                    }
+                }}
+                noValidate
+                autoComplete="off"
+                className='d-flex justify-content-end flex-column'
+            >
+                <Contactfield 
+                    label='Full name'
+                    register={{...register("fullName")}}
+                    error={!!errors.fullName}
+                    helperText={errors.fullName?.message}
+                />
+                <Contactfield 
+                    label='Email'
+                    register={{...register("email")}}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                />
+                <Contactfield 
+                    label='Message'
+                    register={{...register("message")}}
+                    error={!!errors.message}
+                    helperText={errors.message?.message}
+                    multiline={true}
+                    rows={4}
+                />
+                <Button type="submit" variant="contained" color="primary" className='mt-3'>send message</Button>
+            </Box>
+        </>
     );
 }
--- src/pages/components/home/footer.tsx
+++ src/pages/components/home/footer.tsx
@@ -3,9 +3,10 @@ import '../css/footer.css'
 
 function Footer() {
     return (
-        <section className="footer">
-            <div className="container">
-                <p className='text-black'>footer</p>
+        <section className="bg-black opacity-75 mt-5 footer">
+            <div className="container py-4">
+                <p className='fs-4 text-center'><a href='#'>{'<'} Whistner {'/>'}</a></p>
+                <p className='fs-6 text-center'>&copy;Whistner 2023 | All rights reserved.</p>
             </div>
         </section>
     )
